b5782024282c709f41295fef797b9867f6c9feb6
Augusto Pagnossim Frigo <frigo.augusto@gmail.com>
2021-06-12 18:23:14 +0000

    Versao mais ou menos funcional com circulos (ainda nao pronto)

--- haskellt1.hs
+++ haskellt1.hs
@@ -3,6 +3,7 @@ import Text.Printf
 type Point     = (Float,Float)
 type Rect      = (Point,Float,Float)
 type Circle    = (Point,Float)
+type Ellipse = (Point, Float, Float)
 
 -------------------------------------------------------------------------------
 -- Paletas
@@ -14,16 +15,14 @@ type Circle    = (Point,Float)
 sequenceBluePalette:: Int -> [(Int, Int, Int)]
 sequenceBluePalette n = take n $ cycle [(0,0,x) | x <- [0 .. 255]]
 
+sequenceRedPalette:: Int -> [(Int, Int, Int)]
+sequenceRedPalette n = take n $ cycle [(x,0,0) | x <- [120 .. 255-120]]
+
 
 
 -------------------------------------------------------------------------------
 -- Geração de retângulos em suas posições
 -------------------------------------------------------------------------------
--- Vou gerar quadrados e nao retangulos, pois, do contrario, nao e possivel colocar um circulo dentro (na verdade até dá, mas fica feio)
-genRectsInLine :: Int -> Int -> Float-> Float -> Float -> Float -> Float -> Float -> [Rect]
-genRectsInLine 0 column  w h xgap ygap initialx initialy = []
-genRectsInLine line column w h xgap ygap initialx initialy = [((initialx+m*(w+xgap), initialy), w, h) | m <- [0..fromIntegral (line-1)]] ++ genRectsInLine line (column - 1) w h xgap ygap initialx (initialy + h + ygap)
---Essa funcao gera quadrados em linha, depois se chama recursivamente pra fazer tudo de novo em outra coordenada y+h+ygap, a condicao de parada dela eh ter 0 colunas restantes pra desenhar
 
 
 genRectsInDiagonal :: Int -> [Rect]
@@ -31,10 +30,22 @@ genRectsInDiagonal n  = [((m*(w+gap), (m*(w+gap))), w, h) | m <- [0..fromIntegra
   where (w,h) = (3,3)
         gap = 1
 
-genCircles ::  Int -> Int -> Float -> Float -> Float -> Float -> Float -> [Circle]
-genCircles 0 column r xgap ygap initialx initialy = []
 
-genCircles line column r xgap ygap initialx initialy = [((initialx, m*(r+ygap)*initialy), r) | m <- [0..fromIntegral (line-1)]] ++ genCircles (line - 1) column r xgap ygap initialx (initialx + r + xgap)
+
+
+-- Vou gerar quadrados e nao retangulos, pois, do contrario, nao e possivel colocar um circulo dentro (na verdade até dá, mas fica feio)
+genRectsInLine :: Int -> Int -> Float-> Float -> Float -> Float -> Float -> Float -> [Rect]
+genRectsInLine column 0  w h xgap ygap initialx initialy = []
+genRectsInLine column line w h xgap ygap initialx initialy = [((initialx+m*(w+xgap), initialy), w, h) | m <- [0..fromIntegral (column-1)]] ++ genRectsInLine column (line - 1) w h xgap ygap initialx (initialy + h + ygap)
+--Essa funcao gera quadrados em linha, depois se chama recursivamente pra fazer tudo de novo em outra coordenada y+h+ygap, a condicao de parada dela eh ter 0 colunas restantes pra desenhar
+
+
+
+genCircles ::  Int -> Int -> Float -> Float -> Float -> Float -> Float -> Bool -> [Circle]
+genCircles 0 line r xgap ygap initialx initialy fullfill= []
+genCircles column line r xgap ygap initialx initialy False= [] ++ genCircles (column - 1) line r xgap ygap (initialx + 2*r + xgap) initialy True
+
+genCircles column line r xgap ygap initialx initialy True = [((initialx, m*(2*r+ygap)+initialy), r) | m <- [0..fromIntegral (line-1)]] ++ genCircles (column - 1) line r xgap ygap (initialx + 2*r + xgap) initialy False
 -------------------------------------------------------------------------------
 -- Strings SVG
 -------------------------------------------------------------------------------
@@ -47,7 +58,11 @@ svgRect ((x,y),w,h) style =
 
 
 svgCircle :: Circle -> String -> String
-svgCircle ((x,y),r) style = printf "<circle cx='%.3f' cy='%.3f' r='%.2f' />\n" x y r
+svgCircle ((x,y),r) style = printf "<circle cx='%.3f' cy='%.3f' r='%.2f' style='%s' />\n" x y r style
+
+svgEllipse :: Ellipse -> String -> String
+svgEllipse ((x,y), rx, ry) style = printf "<ellipse cx="200" cy="80" rx="100" ry="50"
+  style="fill:yellow;stroke:purple;stroke-width:2" />"
  
 -- String inicial do SVG
 
@@ -77,12 +92,16 @@ main = do
   writeFile "rects.svg" $ svgstrs
   where svgstrs = svgBegin w h ++ svgfinalrects ++ svgfinalcircles ++ svgEnd
         svgfinalrects = svgElements svgRect rects (map svgStyle palette)
-        svgfinalcircles = svgElements svgCircle circles (map svgStyle palette)
-        circles = genCircles 5 10 30 5 10 300 400
+        svgfinalcircles = svgElements svgCircle circles (map svgStyle circlePalette)
+        circles = genCircles   5 10  15   5 10 315 415 True
+        --parametros do circulo: colunas linhas raio xgap ygap (initialx + r) (initialy + r)
         rects = genRectsInLine 5 10 30 30 5 10 300 400
         nrects = 5*10
+        lines = 10
+        columns = 5
+        ncircles = nrects
         palette = sequenceBluePalette nrects
-        --circlePalette = 
+        circlePalette = sequenceRedPalette ncircles
         (w,h) = (5000,5000) -- width,height da imagem SVG
 
 
