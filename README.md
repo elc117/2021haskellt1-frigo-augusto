8c412299d4759f0a1b02e8f24352c58d6b6c77a9
Augusto Pagnossim Frigo <frigo.augusto@gmail.com>
2021-06-13 20:13:26 +0000

    Quase tudo pronto, falta deixar o código mais limpo.

--- README.md
+++ README.md
@@ -2,14 +2,20 @@
 
   ## Objetivo
 
-  O trabalho foi desenvolvido em Haskell e tem como objetivo desenhar um fundo de quadrados espaçados que têm o RGB incrementado do preto ao azul e, quando se chega a 255, volta ao preto, formando um ciclo.
+  O trabalho foi desenvolvido em Haskell e tem como objetivo desenhar um fundo de quadrados espaçados que têm o tom de azul incrementado conforme o input do usuário. Além disso, desenha elipses dentro dos quadrados, que têm seu tom de vermelho incrementado, também de acordo com o input do usuário.
+
+  ## Informações gerais
+
+  É Possível escolher distâncias personalizadas e tamanhos livremente, bem como coordenadas iniciais. Os retângulos com elipses serão sempre desenhados de forma retangular, não sendo possível desenhar escadas, por exemplo. As elipses sempre aparecerão completando os quadrados pelo eixo y e depois completando só o primeiro e o último quadrado do eixo y, ciclicamente.
 
   ## Implementação
 
   O trabalho foi implementado utilizando as funções exemplificadas nas práticas 6 e 4 como base. A lógica foi quase que completamente modificada, mantendo-se apenas as funções de geração do SVG.
   
-  A função que gera o RGB utiliza list comprehension para gerar cores de (0,0,0) a (0,0,255) ciclicamente conforme o numero de elementos.
+  As funções que geram o RGB fazem um ciclo conforme o input do usuário, ou seja, o usuário entra com um valor x que é o inicial no ciclo até 255, e um valor y que é o passo desse ciclo. Para quadrados, utiliza-se (0,0,x) e, para as elipses, (x,0,0).
 
   A função que gera os quadrados faz uma list comprehension para gerar os quadrados no eixo X, e chama-se recursivamente para fazer o mesmo no eixo y, conforme o número de recursões solicitado pelo usuário. A função respeita, também, diferentes "gaps" para os eixos x e y.
 
-  
+  A função que gera as elipses faz algo semelhante à função anterior, mas tem mais condições de não execução. Isso porque o objetivo não é preencher todos os quadrados, mas sim apenas uma parte deles, formando um desenho. Eu havia implementado com círculos anteriormente, mas isso obrigava os retângulos a se tornarem quadrados, do contrário, o círculo ficava para fora. O programa manipula os valores das elipses para encaixá-las, Utiliza-se ry = altura/2 e rx = largura/2, e incrementa-se o valor inicial inserido para o quadrado com o respectivo raio da elipse.
+
+  A main utiliza variáveis auxiliares para traduzir a leitura, pois seria ilegível passar "(read argumento :: Float)" em todos os argumentos de funções. Pega todos os valores que são solicitados ao usuário e gera a imagem completamente baseada neles, sem a utilização de pontos fixos. Só foi utilizado como fixo o tamanho da imagem, 1920x1080.
--- haskellt1.hs
+++ haskellt1.hs
@@ -1,10 +1,15 @@
 import Text.Printf
-import Foreign.C.Types
 
 type Point     = (Float,Float)
 type Rect      = (Point,Float,Float)
-type Circle    = (Point,Float)
 type Ellipsis = (Point, Float, Float)
+
+-------Constantes
+img_width :: Float
+img_width = 1920
+
+img_height :: Float
+img_height = 1080
 
 -------------------------------------------------------------------------------
 -- Paletas
@@ -13,11 +18,11 @@ type Ellipsis = (Point, Float, Float)
 -- O '$' é uma facilidade sintática que substitui parênteses
 -- O cycle é uma função bacana -- procure saber mais sobre ela :-)
 --Agora ele vai ciclicamente do preto ao azul. Quando chega ao 255, volta a 0.
-sequenceBluePalette:: Int -> [(Int, Int, Int)]
-sequenceBluePalette n = take n $ cycle [(0,0,x) | x <- [0 .. 255]]
+sequenceBluePalette:: Int -> Int -> Int -> [(Int, Int, Int)]
+sequenceBluePalette n initial increment = take n $ cycle [(0,0,x) | x <- [initial, initial+increment .. 255]]
 
-sequenceRedPalette:: Int -> [(Int, Int, Int)]
-sequenceRedPalette n = take n $ cycle [(x,0,0) | x <- [120 .. 255-120]]
+sequenceRedPalette:: Int -> Int -> Int -> [(Int, Int, Int)]
+sequenceRedPalette n initial increment = take n $ cycle [(x,0,0) | x <- [initial, initial+increment .. 255]]
 
 
 
@@ -26,27 +31,15 @@ sequenceRedPalette n = take n $ cycle [(x,0,0) | x <- [120 .. 255-120]]
 -------------------------------------------------------------------------------
 
 
-genRectsInDiagonal :: Int -> [Rect]
-genRectsInDiagonal n  = [((m*(w+gap), (m*(w+gap))), w, h) | m <- [0..fromIntegral (n-1)]]
-  where (w,h) = (3,3)
-        gap = 1
-
-
 
 
 
 genRectsInLine :: Int -> Int -> Float-> Float -> Float -> Float -> Float -> Float -> [Rect]
 genRectsInLine column 0  w h xgap ygap initialx initialy = []
-genRectsInLine column line w h xgap ygap initialx initialy = [((initialx+m*(w+xgap), initialy), w, h) | m <- [0..fromIntegral (column-1)]] ++ genRectsInLine column (line - 1) w h xgap ygap initialx (initialy + h + ygap)
+genRectsInLine column line w h xgap ygap initialx initialy = 
+  [((initialx+m*(w+xgap), initialy), w, h) | m <- [0..fromIntegral (column-1)]] ++ genRectsInLine column (line - 1) w h xgap ygap initialx (initialy + h + ygap)
 --Essa funcao gera retangulos em linha, depois se chama recursivamente pra fazer tudo de novo em outra coordenada y+h+ygap, a condicao de parada dela eh ter 0 colunas restantes pra desenhar
 
-
-
-genCircles ::  Int -> Int -> Float -> Float -> Float -> Float -> Float -> Bool -> [Circle]
-genCircles 0 line r xgap ygap initialx initialy fullfill= []
-genCircles column line r xgap ygap initialx initialy False= [] ++ genCircles (column - 1) line r xgap ygap (initialx + 2*r + xgap) initialy True
-
-genCircles column line r xgap ygap initialx initialy True = [((initialx, m*(2*r+ygap)+initialy), r) | m <- [0..fromIntegral (line-1)]] ++ genCircles (column - 1) line r xgap ygap (initialx + 2*r + xgap) initialy False
 
 --------
 
@@ -54,10 +47,12 @@ genCircles column line r xgap ygap initialx initialy True = [((initialx, m*(2*r+
 genEllipsis ::  Int -> Int -> Float -> Float -> Float -> Float -> Float -> Float -> Bool -> [Ellipsis]
 genEllipsis 0 line rx ry xgap ygap initialx initialy fullfill= []
 
-genEllipsis column line rx ry xgap ygap initialx initialy False= [] ++ genEllipsis (column - 1) line rx ry xgap ygap (initialx + 2*rx + xgap) initialy True
+genEllipsis column line rx ry xgap ygap initialx initialy False= 
+  [((initialx, initialy), rx, ry)] ++ [(((initialx, initialy+((fromIntegral (line-1))*(2*ry+ygap))), rx, ry))] ++ genEllipsis (column - 1) line rx ry xgap ygap (initialx + 2*rx + xgap) initialy True
 
-genEllipsis column line rx ry xgap ygap initialx initialy True = [((initialx, m*(2*ry+ygap)+initialy), rx,ry) | m <- [0..fromIntegral (line-1)]] ++ genEllipsis (column - 1) line rx ry xgap ygap (initialx + 2*rx + xgap) initialy False
-
+genEllipsis column line rx ry xgap ygap initialx initialy True = 
+  [((initialx, m*(2*ry+ygap)+initialy), rx,ry) | m <- [0..fromIntegral (line-1)]] ++ genEllipsis (column - 1) line rx ry xgap ygap (initialx + 2*rx + xgap) initialy False
+--Aqui ele vai gerar a lista de elipses e seus raios e coordenadas. Essas coordenadas foram calculadas considerando a posicao dos quadrados. Segue uma logica semelhante a funcao de geracao de retangulos, ajustada para ser feita na direcao do eixo y. O valor booleano da funcao eh o fullfill, que serve pra indicar se a coluna de quadrados vai ser preenchida ou se vai receber elipses somente em seu topo e embaixo.
 -----------------
 
 
@@ -70,10 +65,6 @@ genEllipsis column line rx ry xgap ygap initialx initialy True = [((initialx, m*
 svgRect :: Rect -> String -> String 
 svgRect ((x,y),w,h) style = 
   printf "<rect x='%.3f' y='%.3f' width='%.2f' height='%.2f' style='%s' />\n" x y w h style
-
-
-svgCircle :: Circle -> String -> String
-svgCircle ((x,y),r) style = printf "<circle cx='%.3f' cy='%.3f' r='%.2f' style='%s' />\n" x y r style
 
 svgEllipsis :: Ellipsis -> String -> String
 svgEllipsis ((x,y), rx, ry) style = printf "<ellipse cx='%.3f' cy='%.3f' rx='%.3f' ry='%.3f' style='%s' />" x y rx ry style
@@ -103,30 +94,66 @@ svgElements func elements styles = concat $ zipWith func elements styles
 
 main :: IO ()
 main = do
-  writeFile "rects.svg" $ svgstrs
-  where svgstrs = svgBegin img_width img_height ++ svgfinalrects ++ svgfinalellipsis ++ svgEnd
-        svgfinalrects = svgElements svgRect rects (map svgStyle palette)
+  putStrLn("Quantas linhas de retangulos?(INT)")
+  input_lines <- getLine
+  putStrLn("Quantas colunas de retangulos?(INT)")
+  input_columns <- getLine
+  putStrLn("Qual a coordenada x inicial?(FLOAT)")
+  input_initialx <- getLine
+  putStrLn("Qual a coordenada y inicial?(FLOAT)")
+  input_initialy <- getLine
+  putStrLn("Qual o espaco entre quadrados no eixo x?(FLOAT)")
+  input_xgap <- getLine
+  putStrLn("Qual o espaco entre quadrados no eixo y?(FLOAT)")
+  input_ygap <- getLine
+  putStrLn("Qual a altura dos retangulos?(FLOAT)")
+  input_rect_height <- getLine
+  putStrLn ("Qual a largura dos retangulos?(FLOAT)")
+  input_rect_width <- getLine
+  putStrLn ("Qual o valor inicial do RGB azul?(INT)")
+  input_initial_blueRGB <- getLine
+  putStrLn ("Qual o valor de incremento do RGB azul?(INT)")
+  input_increment_blueRGB <- getLine
+  putStrLn ("Qual o valor inicial do RGB vermelho?(INT)")
+  input_initial_redRGB <- getLine
+  putStrLn("Qual o valor de incremento do RGB vermelho?(INT)")
+  input_increment_redRGB <- getLine
+
+  
+  let   svgstrs = svgBegin img_width img_height ++ svgfinalrects ++ svgfinalellipsis ++ svgEnd
+        svgfinalrects = svgElements svgRect rects (map svgStyle rectPalette)
         svgfinalellipsis = svgElements svgEllipsis ellipsis (map svgStyle ellipsisPalette)
 
-        ellipsis = genEllipsis  columns lines  rx ry xgap ygap (initialx + rx) (initialy + ry) True
+        ellipsis = genEllipsis  columns lines rx ry xgap ygap (initialx + rx) (initialy + ry) True
 
         rects = genRectsInLine columns lines rect_width rect_height xgap ygap initialx initialy
+
+
+        rectPalette = sequenceBluePalette nrects initial_blueRGB increment_blueRGB
+        ellipsisPalette = sequenceRedPalette nellipsis initial_redRGB increment_redRGB
 
         nrects = columns*lines
         rx = rect_width/2
         ry = rect_height/2
-        initialx = 300
-        initialy = 400
-        ygap = 10
-        xgap = 5
-        rect_width = 30
-        rect_height = 20
-        lines = 100
-        columns = 50
-        ncircles = nrects
-        palette = sequenceBluePalette nrects
-        ellipsisPalette = sequenceRedPalette ncircles
-        (img_width,img_height) = (5000,5000) -- width,height da imagem SVG
+        nellipsis = nrects
+
+        initialx = read input_initialx :: Float
+        initialy = read input_initialy :: Float
+        xgap = read input_xgap :: Float
+        ygap = read input_ygap :: Float
+        rect_width = read input_rect_width :: Float
+        rect_height = read input_rect_height :: Float
+        lines = read input_lines :: Int
+        columns = read input_columns :: Int
+        initial_blueRGB = read input_initial_blueRGB :: Int
+        increment_blueRGB = read input_increment_blueRGB :: Int
+        initial_redRGB = read input_initial_redRGB :: Int
+        increment_redRGB = read input_increment_redRGB :: Int
+
+
+
+  writeFile "rects.svg" $ svgstrs
+
 
 
 
